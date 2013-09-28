76fc3dd91735af38acadfa79ef41c3d6e68ef0b4
Kevin Mehall <km@kevinmehall.net>
2013-09-28 17:10:54 -0400

    Remove javascript bootstrap and re-structure to rustpkg workspace.

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-./rustpeg.sh examples/tests.rustpeg > examples/test_grammar.rs
+bin/peg examples/tests.rustpeg > examples/test_grammar.rs
 rust test examples/tests.rs
 
-./rustpeg.sh examples/arithmetic.rustpeg > examples/arithmetic.rs
+bin/peg examples/arithmetic.rustpeg > examples/arithmetic.rs
 rust run examples/test_arithmetic.rs
