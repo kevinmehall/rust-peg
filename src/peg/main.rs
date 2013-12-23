c2ddf610053358ec60d66659e3db87dd27e1696d
Kevin Mehall <km@kevinmehall.net>
2013-12-23 11:02:49 -0700

    Fix for from_utf8 change

--- src/peg/main.rs
+++ src/peg/main.rs
@@ -14,7 +14,8 @@ mod grammar;
 
 fn main() {
 	let args = os::args();
-	let source = str::from_utf8(File::open(&Path::new(args[1])).read_to_end());
+	let source_utf8 = File::open(&Path::new(args[1])).read_to_end();
+	let source = str::from_utf8(source_utf8);
 	let grammar_def = grammar::grammar(source).unwrap();
 	let w = RustWriter::new(stdout());
 	compile_grammar(&w, grammar_def);
