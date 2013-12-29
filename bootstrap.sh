d631e82ba6446035c459bbbcae1024f4af0aa8bd
Kevin Mehall <km@kevinmehall.net>
2013-12-28 17:07:19 -0700

    Clean up

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,14 @@
 set -e
 
 rustpkg clean peg
-rustpkg install peg | grep -v workcache
+rustpkg install peg
 bin/peg src/peg/grammar.rustpeg > src/peg/grammar_new.rs
 
 mv src/peg/grammar.rs src/peg/grammar_old.rs
 mv src/peg/grammar_new.rs src/peg/grammar.rs
 
 rustpkg clean peg
-rustpkg install peg | grep -v workcache
+rustpkg install peg
 bin/peg src/peg/grammar.rustpeg > src/peg/grammar_new.rs
 diff -qs src/peg/grammar.rs src/peg/grammar_new.rs
 
