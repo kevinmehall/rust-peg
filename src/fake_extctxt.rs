6f978cabd6f20dad1f2915b0d95412b61fd7a423
Logan Buckley <lbuckley@opinionlab.com>
2015-08-25 23:22:16 -0500

    Add newly-required additional argument to ExtCtxt::new().

--- src/fake_extctxt.rs
+++ src/fake_extctxt.rs
@@ -1,13 +1,16 @@
 use syntax;
 use syntax::codemap::DUMMY_SP;
 use syntax::ext::base::ExtCtxt;
+use syntax::feature_gate::GatedCfg;
 
 /// Create a fake ExtCtxt to perform macro quasiquotes outside of rustc plugins
 pub fn with_fake_extctxt<T, F: Fn(&ExtCtxt) -> T>(f: F) -> T {
   let ps = syntax::parse::ParseSess::new();
+  let mut fg_cfg = Vec::<GatedCfg>::new();
 
   let mut cx = syntax::ext::base::ExtCtxt::new(&ps, Vec::new(),
-    syntax::ext::expand::ExpansionConfig::default("rust-peg".to_string())
+    syntax::ext::expand::ExpansionConfig::default("rust-peg".to_string()),
+    &mut fg_cfg
   );
 
   cx.bt_push(syntax::codemap::ExpnInfo{
