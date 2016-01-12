33c9f828fa5e8bad59e196c8669ace89991c9c53
Kevin Mehall <km@kevinmehall.net>
2016-01-11 18:15:04 -0800

    rust: parser API change

--- src/peg_syntax_ext.rs
+++ src/peg_syntax_ext.rs
@@ -94,7 +94,7 @@ fn parse_arg(cx: &mut ExtCtxt, tts: &[ast::TokenTree]) -> Option<String> {
         ast::ExprLit(ref spanned) => {
             match spanned.node {
                 ast::LitStr(ref n, _) => {
-                    if !panictry!(parser.eat(&token::Eof)) {
+                    if !parser.eat(&token::Eof) {
                         cx.span_err(parser.span,
                                     "expected only one string literal");
                         return None
