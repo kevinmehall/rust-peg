3f66b26a9b190a83f661bc8ef812461f15b9102d
Isaac van Bakel <ivb@vanbakel.io>
2017-11-22 00:20:40 +0000

    Added tests to check subgrammar compilation
    Subgrammars without arguments are tested.
    Subgrammars with arguments using some converter are also tested.

--- peg-tests/build.rs
+++ peg-tests/build.rs
@@ -1,5 +1,7 @@
 extern crate peg;
 
 fn main() {
+    peg::cargo_build("src/test_subgrammar.rustpeg");
+    peg::cargo_build("src/test_subgrammar_with_args.rustpeg");
     peg::cargo_build("src/test_grammar.rustpeg");
 }
