9796fab62afc7b300ec6e101b8544da9ca2bbe84
Kevin Mehall <km@kevinmehall.net>
2017-12-16 12:03:13 -0800

    peg-syntax-ext: test peg_file!() macro

--- peg-syntax-ext/tests/test_arithmetic_ast.rs
+++ peg-syntax-ext/tests/test_arithmetic_ast.rs
@@ -9,28 +9,7 @@ pub enum Expression {
 	Product(Box<Expression>, Box<Expression>),
 }
 
-peg! arithmetic(r#"
-use super::Expression;
-
-#[pub]
-expression -> Expression
-	= sum
-
-sum -> Expression
-	= l:product "+" r:product { Expression::Sum(Box::new(l), Box::new(r)) }
-	/ product
-
-product -> Expression
-	= l:atom "*" r:atom { Expression::Product(Box::new(l), Box::new(r)) }
-	/ atom
-
-atom -> Expression
-	= number
-	/ "(" v:sum ")" { v }
-
-number -> Expression
-	= n:$([0-9]+) { Expression::Number(n.parse().unwrap()) }
-"#);
+peg_file! arithmetic("arithmetic_ast.rustpeg");
 
 #[test]
 fn main() {
