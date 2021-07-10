kevinmehall/rust-peg

commit 202f978d4466fc76e2cea28065e47b87e8433c58

Put grammar lifetime params on ParseState so they work with #[cache]
Fixes #264

peg-macros/grammar.rs
@@ -4,8 +4,9 @@ pub mod peg {
     use super::*;
     type Input = FlatTokenStream;
     type PositionRepr = <Input as ::peg::Parse>::PositionRepr;
+    #[allow(unused_parens)]
     struct ParseState<'input> {
-        _phantom: ::std::marker::PhantomData<&'input ()>,
+        _phantom: ::std::marker::PhantomData<(&'input ())>,
         primary_cache: ::std::collections::HashMap<usize, ::peg::RuleResult<SpannedExpr>>,
     }
     impl<'input> ParseState<'input> {

peg-macros/translate.rs
@@ -143,6 +143,7 @@ pub(crate) fn compile_grammar(grammar: &Grammar) -> TokenStream {
 
 fn make_parse_state(grammar: &Grammar) -> TokenStream {
     let span = Span::mixed_site();
+    let grammar_ty_params = ty_params_slice(&grammar.ty_params);
     let mut cache_fields_def: Vec<TokenStream> = Vec::new();
     let mut cache_fields: Vec<Ident> = Vec::new();
     for rule in grammar.iter_rules() {
@@ -157,13 +158,14 @@ fn make_parse_state(grammar: &Grammar) -> TokenStream {
     }
 
     quote_spanned! { span =>
-        struct ParseState<'input> {
-            _phantom: ::std::marker::PhantomData<&'input ()>,
+        #[allow(unused_parens)]
+        struct ParseState<'input #(, #grammar_ty_params)*> {
+            _phantom: ::std::marker::PhantomData<(&'input () #(, &#grammar_ty_params ())*)>,
             #(#cache_fields_def),*
         }
 
-        impl<'input> ParseState<'input> {
-            fn new() -> ParseState<'input> {
+        impl<'input#(, #grammar_ty_params)*> ParseState<'input #(, #grammar_ty_params)*> {
+            fn new() -> ParseState<'input #(, #grammar_ty_params)*> {
                 ParseState {
                     _phantom: ::std::marker::PhantomData,
                     #(#cache_fields: ::std::collections::HashMap::new()),*
@@ -185,7 +187,7 @@ fn rule_params_list(context: &Context, rule: &Rule) -> Vec<TokenStream> {
         match &param.ty {
             RuleParamTy::Rust(ty) => quote_spanned!{ span => #name: #ty },
             RuleParamTy::Rule(ty) => quote_spanned!{ span =>
-                #name: impl Fn(&'input Input<#(#extra_ty_params),*>, &mut ParseState<'input>, &mut ::peg::error::ErrorState, usize) -> ::peg::RuleResult<#ty>
+                #name: impl Fn(&'input Input<#(#extra_ty_params),*>, &mut ParseState<'input #(, #extra_ty_params)*>, &mut ::peg::error::ErrorState, usize) -> ::peg::RuleResult<#ty>
             },
         }
     }).collect()
@@ -233,7 +235,7 @@ fn compile_rule(context: &Context, rule: &Rule) -> TokenStream {
 
     let rule_params = rule_params_list(&context, rule);
 
-    if rule.cached {
+    let fn_body = if rule.cached {
         let cache_field = format_ident!("{}_cache", rule.name);
 
         let cache_trace = if cfg!(feature = "trace") {
@@ -250,23 +252,22 @@ fn compile_rule(context: &Context, rule: &Rule) -> TokenStream {
         };
 
         quote_spanned! { span =>
-            fn #name<'input #(, #extra_ty_params)* #(, #ty_params)*>(__input: &'input Input<#(#extra_ty_params),*>, __state: &mut ParseState<'input>, __err_state: &mut ::peg::error::ErrorState, __pos: usize #extra_args_def #(, #rule_params)*) -> ::peg::RuleResult<#ret_ty> {
-                #![allow(non_snake_case, unused)]
-                if let Some(entry) = __state.#cache_field.get(&__pos) {
-                    #cache_trace
-                    return entry.clone();
-                }
-                let __rule_result = #wrapped_body;
-                __state.#cache_field.insert(__pos, __rule_result.clone());
-                __rule_result
+            if let Some(entry) = __state.#cache_field.get(&__pos) {
+                #cache_trace
+                return entry.clone();
             }
+            let __rule_result = #wrapped_body;
+            __state.#cache_field.insert(__pos, __rule_result.clone());
+            __rule_result
         }
     } else {
-        quote_spanned! { span =>
-            fn #name<'input #(, #extra_ty_params)* #(, #ty_params)*>(__input: &'input Input<#(#extra_ty_params),*>, __state: &mut ParseState<'input>, __err_state: &mut ::peg::error::ErrorState, __pos: usize #extra_args_def #(, #rule_params)*) -> ::peg::RuleResult<#ret_ty> {
-                #![allow(non_snake_case, unused)]
-                #wrapped_body
-            }
+        wrapped_body
+    };
+
+    quote_spanned! { span =>
+        fn #name<'input #(, #extra_ty_params)* #(, #ty_params)*>(__input: &'input Input<#(#extra_ty_params),*>, __state: &mut ParseState<'input #(, #extra_ty_params)*>, __err_state: &mut ::peg::error::ErrorState, __pos: usize #extra_args_def #(, #rule_params)*) -> ::peg::RuleResult<#ret_ty> {
+            #![allow(non_snake_case, unused)]
+            #fn_body
         }
     }
 }

tests/run-pass/lifetimes.rs
@@ -10,7 +10,14 @@ peg::parser!{
 
         rule list() -> Vec<&'t str> = [Token("(")] l:commasep(<string()>) [Token(")")] { l }
         rule string() -> &'t str = [Token(inner)] { inner }
+
+        #[cache]
+        rule cached() -> Token<'t> = [a] { a }
     }
+}
+
+peg::parser!{
+    grammar unused_args<'a>() for () { }
 }
 
 fn main() {

commit eccdfa1f728b37bbe7ac894b18c710c594dd7e62

Allow only lifetime, not type, parameters at the grammar level
Type parameters were broken by 202f978, and were previously untested and
somewhat broken when it came to ordering them relative to lifetime
parameters in rule functions' generic lists. In theory they could be
supported by handling lifetime and type parameters separately for both
of these cases, but I don't see a strong use case for them.

Techincally a breaking change, but I highly doubt it was ever used.

peg-macros/ast.rs
@@ -5,7 +5,7 @@ pub struct Grammar {
     pub doc: Option<TokenStream>,
     pub visibility: Option<TokenStream>,
     pub name: Ident,
-    pub ty_params: Option<Vec<TokenStream>>,
+    pub lifetime_params: Option<Vec<TokenStream>>,
     pub args: Vec<(Ident, TokenStream)>,
     pub items: Vec<Item>,
     pub input_type: TokenStream,

peg-macros/grammar.rs
@@ -85,7 +85,7 @@ pub mod peg {
                                         __parse_IDENT(__input, __state, __err_state, __pos);
                                     match __seq_res {
                                         ::peg::RuleResult::Matched(__pos, name) => {
-                                            let __seq_res = match __parse_rust_ty_params(
+                                            let __seq_res = match __parse_rust_lifetime_params(
                                                 __input,
                                                 __state,
                                                 __err_state,
@@ -102,14 +102,17 @@ pub mod peg {
                                                 }
                                             };
                                             match __seq_res {
-                                                ::peg::RuleResult::Matched(__pos, ty_params) => {
+                                                ::peg::RuleResult::Matched(
+                                                    __pos,
+                                                    lifetime_params,
+                                                ) => {
                                                     let __seq_res = __parse_grammar_args(
                                                         __input,
                                                         __state,
                                                         __err_state,
                                                         __pos,
                                                     );
-                                                    match __seq_res { :: peg :: RuleResult :: Matched (__pos , args) => { match :: peg :: ParseLiteral :: parse_string_literal (__input , __pos , "for") { :: peg :: RuleResult :: Matched (__pos , __val) => { { let __seq_res = { let str_start = __pos ; match match __parse_rust_type (__input , __state , __err_state , __pos) { :: peg :: RuleResult :: Matched (pos , _) => :: peg :: RuleResult :: Matched (pos , ()) , :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } { :: peg :: RuleResult :: Matched (__newpos , _) => { :: peg :: RuleResult :: Matched (__newpos , :: peg :: ParseSlice :: parse_slice (__input , str_start , __newpos)) } , :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } } ; match __seq_res { :: peg :: RuleResult :: Matched (__pos , input_type) => { match :: peg :: ParseLiteral :: parse_string_literal (__input , __pos , "{") { :: peg :: RuleResult :: Matched (__pos , __val) => { { let __seq_res = { let mut __repeat_pos = __pos ; let mut __repeat_value = vec ! () ; loop { let __pos = __repeat_pos ; let __step_res = __parse_item (__input , __state , __err_state , __pos) ; match __step_res { :: peg :: RuleResult :: Matched (__newpos , __value) => { __repeat_pos = __newpos ; __repeat_value . push (__value) ; } , :: peg :: RuleResult :: Failed => { break ; } } } :: peg :: RuleResult :: Matched (__repeat_pos , __repeat_value) } ; match __seq_res { :: peg :: RuleResult :: Matched (__pos , items) => { match :: peg :: ParseLiteral :: parse_string_literal (__input , __pos , "}") { :: peg :: RuleResult :: Matched (__pos , __val) => { :: peg :: RuleResult :: Matched (__pos , (|| { Grammar { doc , visibility , name , ty_params , args , input_type , items } }) ()) } :: peg :: RuleResult :: Failed => { __err_state . mark_failure (__pos , "\"}\"") ; :: peg :: RuleResult :: Failed } } } :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } } } :: peg :: RuleResult :: Failed => { __err_state . mark_failure (__pos , "\"{\"") ; :: peg :: RuleResult :: Failed } } } :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } } } :: peg :: RuleResult :: Failed => { __err_state . mark_failure (__pos , "\"for\"") ; :: peg :: RuleResult :: Failed } } } :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , }
+                                                    match __seq_res { :: peg :: RuleResult :: Matched (__pos , args) => { match :: peg :: ParseLiteral :: parse_string_literal (__input , __pos , "for") { :: peg :: RuleResult :: Matched (__pos , __val) => { { let __seq_res = { let str_start = __pos ; match match __parse_rust_type (__input , __state , __err_state , __pos) { :: peg :: RuleResult :: Matched (pos , _) => :: peg :: RuleResult :: Matched (pos , ()) , :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } { :: peg :: RuleResult :: Matched (__newpos , _) => { :: peg :: RuleResult :: Matched (__newpos , :: peg :: ParseSlice :: parse_slice (__input , str_start , __newpos)) } , :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } } ; match __seq_res { :: peg :: RuleResult :: Matched (__pos , input_type) => { match :: peg :: ParseLiteral :: parse_string_literal (__input , __pos , "{") { :: peg :: RuleResult :: Matched (__pos , __val) => { { let __seq_res = { let mut __repeat_pos = __pos ; let mut __repeat_value = vec ! () ; loop { let __pos = __repeat_pos ; let __step_res = __parse_item (__input , __state , __err_state , __pos) ; match __step_res { :: peg :: RuleResult :: Matched (__newpos , __value) => { __repeat_pos = __newpos ; __repeat_value . push (__value) ; } , :: peg :: RuleResult :: Failed => { break ; } } } :: peg :: RuleResult :: Matched (__repeat_pos , __repeat_value) } ; match __seq_res { :: peg :: RuleResult :: Matched (__pos , items) => { match :: peg :: ParseLiteral :: parse_string_literal (__input , __pos , "}") { :: peg :: RuleResult :: Matched (__pos , __val) => { :: peg :: RuleResult :: Matched (__pos , (|| { Grammar { doc , visibility , name , lifetime_params , args , input_type , items } }) ()) } :: peg :: RuleResult :: Failed => { __err_state . mark_failure (__pos , "\"}\"") ; :: peg :: RuleResult :: Failed } } } :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } } } :: peg :: RuleResult :: Failed => { __err_state . mark_failure (__pos , "\"{\"") ; :: peg :: RuleResult :: Failed } } } :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , } } } :: peg :: RuleResult :: Failed => { __err_state . mark_failure (__pos , "\"for\"") ; :: peg :: RuleResult :: Failed } } } :: peg :: RuleResult :: Failed => :: peg :: RuleResult :: Failed , }
                                                 }
                                                 ::peg::RuleResult::Failed => {
                                                     ::peg::RuleResult::Failed
@@ -129,6 +132,95 @@ pub mod peg {
                     }
                 }
                 ::peg::RuleResult::Failed => ::peg::RuleResult::Failed,
+            }
+        }
+    }
+    fn __parse_rust_lifetime_params<'input>(
+        __input: &'input Input,
+        __state: &mut ParseState<'input>,
+        __err_state: &mut ::peg::error::ErrorState,
+        __pos: usize,
+    ) -> ::peg::RuleResult<Vec<TokenStream>> {
+        #![allow(non_snake_case, unused)]
+        match ::peg::ParseLiteral::parse_string_literal(__input, __pos, "<") {
+            ::peg::RuleResult::Matched(__pos, __val) => {
+                let __seq_res = {
+                    let mut __repeat_pos = __pos;
+                    let mut __repeat_value = vec![];
+                    loop {
+                        let __pos = __repeat_pos;
+                        let __pos = if __repeat_value.is_empty() {
+                            __pos
+                        } else {
+                            let __sep_res = match ::peg::ParseLiteral::parse_string_literal(
+                                __input, __pos, ",",
+                            ) {
+                                ::peg::RuleResult::Matched(__pos, __val) => {
+                                    ::peg::RuleResult::Matched(__pos, __val)
+                                }
+                                ::peg::RuleResult::Failed => {
+                                    __err_state.mark_failure(__pos, "\",\"");
+                                    ::peg::RuleResult::Failed
+                                }
+                            };
+                            match __sep_res {
+                                ::peg::RuleResult::Matched(__newpos, _) => __newpos,
+                                ::peg::RuleResult::Failed => break,
+                            }
+                        };
+                        let __step_res = {
+                            let str_start = __pos;
+                            match match __parse_LIFETIME(__input, __state, __err_state, __pos) {
+                                ::peg::RuleResult::Matched(pos, _) => {
+                                    ::peg::RuleResult::Matched(pos, ())
+                                }
+                                ::peg::RuleResult::Failed => ::peg::RuleResult::Failed,
+                            } {
+                                ::peg::RuleResult::Matched(__newpos, _) => {
+                                    ::peg::RuleResult::Matched(
+                                        __newpos,
+                                        ::peg::ParseSlice::parse_slice(
+                                            __input, str_start, __newpos,
+                                        ),
+                                    )
+                                }
+                                ::peg::RuleResult::Failed => ::peg::RuleResult::Failed,
+                            }
+                        };
+                        match __step_res {
+                            ::peg::RuleResult::Matched(__newpos, __value) => {
+                                __repeat_pos = __newpos;
+                                __repeat_value.push(__value);
+                            }
+                            ::peg::RuleResult::Failed => {
+                                break;
+                            }
+                        }
+                    }
+                    if __repeat_value.len() >= 1 {
+                        ::peg::RuleResult::Matched(__repeat_pos, __repeat_value)
+                    } else {
+                        ::peg::RuleResult::Failed
+                    }
+                };
+                match __seq_res {
+                    ::peg::RuleResult::Matched(__pos, p) => {
+                        match ::peg::ParseLiteral::parse_string_literal(__input, __pos, ">") {
+                            ::peg::RuleResult::Matched(__pos, __val) => {
+                                ::peg::RuleResult::Matched(__pos, (|| p)())
+                            }
+                            ::peg::RuleResult::Failed => {
+                                __err_state.mark_failure(__pos, "\">\"");
+                                ::peg::RuleResult::Failed
+                            }
+                        }
+                    }
+                    ::peg::RuleResult::Failed => ::peg::RuleResult::Failed,
+                }
+            }
+            ::peg::RuleResult::Failed => {
+                __err_state.mark_failure(__pos, "\"<\"");
+                ::peg::RuleResult::Failed
             }
         }
     }

peg-macros/grammar.rustpeg
@@ -6,8 +6,11 @@ use crate::tokens::FlatTokenStream;
 use proc_macro2::{ TokenStream, Ident, Group, Literal, Delimiter, Span };
 
 pub rule peg_grammar() -> Grammar
-    = doc:rust_doc_comment() visibility:rust_visibility() "grammar" name:IDENT() ty_params:rust_ty_params()? args:grammar_args() "for" input_type:$(rust_type()) "{" items:item()* "}"
-        { Grammar { doc, visibility, name, ty_params, args, input_type, items } }
+    = doc:rust_doc_comment() visibility:rust_visibility() "grammar" name:IDENT() lifetime_params:rust_lifetime_params()? args:grammar_args() "for" input_type:$(rust_type()) "{" items:item()* "}"
+        { Grammar { doc, visibility, name, lifetime_params, args, input_type, items } }
+
+    rule rust_lifetime_params() -> Vec<TokenStream>
+        = "<" p:(($(LIFETIME())) ++ ",") ">" { p }
 
 rule grammar_args() -> Vec<(Ident, TokenStream)>
     = "(" args:((i:IDENT() ":" t:$(rust_type()) { (i, t) })**",") ","? ")" { args }

peg-macros/translate.rs
@@ -49,7 +49,7 @@ fn extra_args_call(grammar: &Grammar) -> TokenStream {
 struct Context<'a> {
     rules: &'a HashMap<String, &'a Rule>,
     rules_from_args: HashSet<String>,
-    grammar_ty_params: &'a [TokenStream],
+    grammar_lifetime_params: &'a [TokenStream],
     input_ty: TokenStream,
     parse_state_ty: TokenStream,
     extra_args_call: TokenStream,
@@ -62,14 +62,14 @@ pub(crate) fn compile_grammar(grammar: &Grammar) -> TokenStream {
 
     let analysis = analysis::check(&grammar);
 
-    let grammar_ty_params = ty_params_slice(&grammar.ty_params);
+    let grammar_lifetime_params = ty_params_slice(&grammar.lifetime_params);
 
     let context = &Context {
         rules: &analysis.rules,
         rules_from_args: HashSet::new(),
-        grammar_ty_params: grammar_ty_params,
-        input_ty: quote!(&'input Input<#(#grammar_ty_params),*>),
-        parse_state_ty: quote!(&mut ParseState<'input #(, #grammar_ty_params)*>),
+        grammar_lifetime_params: grammar_lifetime_params,
+        input_ty: quote!(&'input Input<#(#grammar_lifetime_params),*>),
+        parse_state_ty: quote!(&mut ParseState<'input #(, #grammar_lifetime_params)*>),
         extra_args_call: extra_args_call(grammar),
         extra_args_def: extra_args_def(grammar),
     };
@@ -121,7 +121,6 @@ pub(crate) fn compile_grammar(grammar: &Grammar) -> TokenStream {
 
     let doc = &grammar.doc;
     let input_type = &grammar.input_type;
-    let input_ty_params = ty_params_slice(&grammar.ty_params);
     let visibility = &grammar.visibility;
 
     let mut errors = Vec::new();
@@ -139,8 +138,8 @@ pub(crate) fn compile_grammar(grammar: &Grammar) -> TokenStream {
         #visibility mod #name {
             #[allow(unused_imports)]
             use super::*;
-            type Input<#(#input_ty_params),*> = #input_type;
-            type PositionRepr<#(#input_ty_params),*> = <Input<#(#input_ty_params),*> as ::peg::Parse>::PositionRepr;
+            type Input<#(#grammar_lifetime_params),*> = #input_type;
+            type PositionRepr<#(#grammar_lifetime_params),*> = <Input<#(#grammar_lifetime_params),*> as ::peg::Parse>::PositionRepr;
 
             #(#errors)*
             #(#items)*
@@ -150,7 +149,7 @@ pub(crate) fn compile_grammar(grammar: &Grammar) -> TokenStream {
 
 fn make_parse_state(grammar: &Grammar) -> TokenStream {
     let span = Span::mixed_site();
-    let grammar_ty_params = ty_params_slice(&grammar.ty_params);
+    let grammar_lifetime_params = ty_params_slice(&grammar.lifetime_params);
     let mut cache_fields_def: Vec<TokenStream> = Vec::new();
     let mut cache_fields: Vec<Ident> = Vec::new();
     for rule in grammar.iter_rules() {
@@ -166,13 +165,13 @@ fn make_parse_state(grammar: &Grammar) -> TokenStream {
 
     quote_spanned! { span =>
         #[allow(unused_parens)]
-        struct ParseState<'input #(, #grammar_ty_params)*> {
-            _phantom: ::std::marker::PhantomData<(&'input () #(, &#grammar_ty_params ())*)>,
+        struct ParseState<'input #(, #grammar_lifetime_params)*> {
+            _phantom: ::std::marker::PhantomData<(&'input () #(, &#grammar_lifetime_params ())*)>,
             #(#cache_fields_def),*
         }
 
-        impl<'input #(, #grammar_ty_params)*> ParseState<'input #(, #grammar_ty_params)*> {
-            fn new() -> ParseState<'input #(, #grammar_ty_params)*> {
+        impl<'input #(, #grammar_lifetime_params)*> ParseState<'input #(, #grammar_lifetime_params)*> {
+            fn new() -> ParseState<'input #(, #grammar_lifetime_params)*> {
                 ParseState {
                     _phantom: ::std::marker::PhantomData,
                     #(#cache_fields: ::std::collections::HashMap::new()),*
@@ -210,7 +209,7 @@ fn compile_rule(context: &Context, rule: &Rule) -> TokenStream {
     let ty_params = ty_params_slice(&rule.ty_params);
     let input_ty = &context.input_ty;
     let parse_state_ty = &context.parse_state_ty;
-    let grammar_ty_params = context.grammar_ty_params;
+    let grammar_lifetime_params = context.grammar_lifetime_params;
 
     let mut context = context.clone();
     context
@@ -275,7 +274,7 @@ fn compile_rule(context: &Context, rule: &Rule) -> TokenStream {
     };
 
     quote_spanned! { span =>
-        fn #name<'input #(, #grammar_ty_params)* #(, #ty_params)*>(__input: #input_ty, __state: #parse_state_ty, __err_state: &mut ::peg::error::ErrorState, __pos: usize #extra_args_def #(, #rule_params)*) -> ::peg::RuleResult<#ret_ty> {
+        fn #name<'input #(, #grammar_lifetime_params)* #(, #ty_params)*>(__input: #input_ty, __state: #parse_state_ty, __err_state: &mut ::peg::error::ErrorState, __pos: usize #extra_args_def #(, #rule_params)*) -> ::peg::RuleResult<#ret_ty> {
             #![allow(non_snake_case, unused)]
             #fn_body
         }
@@ -290,7 +289,7 @@ fn compile_rule_export(context: &Context, rule: &Rule) -> TokenStream {
     let visibility = &rule.visibility;
     let parse_fn = format_ident!("__parse_{}", rule.name.to_string(), span = name.span());
     let ty_params = ty_params_slice(&rule.ty_params);
-    let grammar_ty_params = context.grammar_ty_params;
+    let grammar_lifetime_params = context.grammar_lifetime_params;
     let input_ty = &context.input_ty;
     let rule_params = rule_params_list(context, rule);
     let rule_params_call: Vec<TokenStream> = rule
@@ -312,7 +311,7 @@ fn compile_rule_export(context: &Context, rule: &Rule) -> TokenStream {
 
     quote_spanned! { span =>
         #doc
-        #visibility fn #name<'input #(, #grammar_ty_params)* #(, #ty_params)*>(__input: #input_ty #extra_args_def #(, #rule_params)*) -> ::std::result::Result<#ret_ty, ::peg::error::ParseError<PositionRepr<#(#grammar_ty_params),*>>> {
+        #visibility fn #name<'input #(, #grammar_lifetime_params)* #(, #ty_params)*>(__input: #input_ty #extra_args_def #(, #rule_params)*) -> ::std::result::Result<#ret_ty, ::peg::error::ParseError<PositionRepr<#(#grammar_lifetime_params),*>>> {
             #![allow(non_snake_case, unused)]
 
             let mut __err_state = ::peg::error::ErrorState::new(::peg::Parse::start(__input));