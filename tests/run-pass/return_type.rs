8a40aa35f32fa573f2dc99756b9f687dee38767e
Kevin Mehall <km@kevinmehall.net>
2022-10-17 22:28:33 -0600

    Disable `impl Trait` test with --features trace
    That didn't work due to naming the type in `__peg_result` previously,
    and I don't see a good way to avoid that without breaking other things.

--- tests/run-pass/return_type.rs
+++ tests/run-pass/return_type.rs
@@ -1,5 +1,7 @@
 use std::fmt::Debug;
 
+// `--features trace` code names the return type, so doesn't work with `impl Trait`
+#[cfg(not(feature = "trace"))]
 peg::parser!{
     grammar g() for str {
         pub rule returns_impl_trait() -> impl Debug
@@ -8,5 +10,6 @@ peg::parser!{
 }
 
 fn main() {
+    #[cfg(not(feature = "trace"))]
     assert_eq!(format!("{:?}", g::returns_impl_trait("")), "Ok(5)");
 }
