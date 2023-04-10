9cfc49e7264885f59aed05635957e61bc166a879
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-04-11 00:14:01 +0800

    add a tracing test case

---
+++ tests/run-pass/tracing.rs
@@ -0,0 +1,72 @@
+extern crate peg;
+
+peg::parser!(grammar parser() for str {
+    // JSON grammar (RFC 4627). Note that this only checks for valid JSON and does not build a syntax
+    // tree.
+
+    pub rule json() = _ (object() / array()) _
+
+    rule _() = [' ' | '\t' | '\r' | '\n']*
+    rule value_separator() = _ "," _
+
+    rule value()
+        = "false" / "true" / "null" / object() / array() / number() / string()
+
+    rule object()
+        = "{" _ member() ** value_separator() _ "}"
+
+    rule member()
+        = string() _ ":" _ value()
+
+    rule array()
+        = "[" _ (value() ** value_separator()) _ "]"
+
+    rule number()
+        = "-"? int() frac()? exp()? {}
+
+    rule int()
+        = ['0'] / ['1'..='9']['0'..='9']*
+
+    rule exp()
+        = ("e" / "E") ("-" / "+")? ['0'..='9']*<1,>
+
+    rule frac()
+        = "." ['0'..='9']*<1,>
+
+    // note: escaped chars not handled
+    rule string()
+        = "\"" (!"\"" [_])* "\""
+});
+
+fn main() {
+    #[cfg(feature = "trace")]
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .json()
+        .init();
+
+    let input = r#"
+{
+	"X": 0.6e2,
+	"Y": 5,
+	"Z": -5.312344,
+	"Bool": false,
+	"Bool": true,
+	"Null": null,
+	"Attr": {
+		"Name": "bla",
+		"Siblings": [6, 1, 2, {}, {}, {}]
+	},
+	"Nested Array": [[[[[[[[[]]]]]]]]],
+	"Obj": {
+		"Child": {
+			"A": [],
+			"Child": {
+				"Child": {}
+			}
+		}
+	}
+}
+"#;
+    parser::json(input).unwrap();
+}
