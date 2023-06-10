a3d2e47b84ca1d9243a873e7c4e977ea5cdd63a7
Kevin Mehall <km@kevinmehall.net>
2023-06-10 15:56:21 -0600

    Run compile-fail tests only on designated version

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -1,20 +1,26 @@
-extern crate trybuild;
-
 fn main() {
+    let args: Vec<_> = std::env::args().collect();
     let t = trybuild::TestCases::new();
 
-    let rust_ver = option_env!("TRAVIS_RUST_VERSION");
-    if rust_ver.is_none() {
-        println!(
-            "Note: compile-fail tests are normally only tested on the stable rust compiler in CI."
-        );
-    }
-
-    if rust_ver.is_none() || rust_ver == Some("stable") {
-        t.compile_fail("tests/compile-fail/*.rs");
-    } else {
-        println!("Skipping compile-fail tests.");
-    }
-
     t.pass("tests/run-pass/*.rs");
+
+    let expected_rust_ver = env!("CARGO_PKG_RUST_VERSION");
+    let run_anyway = args.iter().any(|a| a == "--compile-fail");
+
+    let run_compile_fail = run_anyway || version_check::is_exact_version(expected_rust_ver).unwrap_or(true);
+    if run_compile_fail {
+        t.compile_fail("tests/compile-fail/*.rs");
+    }
+
+    // Trybuild runs the configured tests on drop
+    drop(t);
+
+    if !run_compile_fail {
+        eprintln!("!!! Skipped compile-fail tests !!!");
+        eprintln!("These tests are only checked on rust version {expected_rust_ver} because");
+        eprintln!("the error message text may change between compiler versions.");
+        eprintln!("");
+        eprintln!("Run `cargo +{expected_rust_ver} test` to run these tests.");
+        eprintln!("");
+    }
 }
