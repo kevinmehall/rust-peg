70daa43a62f9838922da4e2b478e4467e557f0f5
Andrew Riachi <andrew.riachi@gmail.com>
2024-11-24 19:47:26 -0600

    fix test

--- tests/run-pass/assembly_ast_dyn_type_param_bounds.rs
+++ tests/run-pass/assembly_ast_dyn_type_param_bounds.rs
@@ -2,60 +2,72 @@ extern crate peg;
 use peg::parser;
 
 // C++ in Rust
-pub trait Operation<'a>: std::fmt::Debug + PartialEq + Eq + Clone{}
-pub trait Operand<'a>: std::fmt::Debug + PartialEq + Eq + Clone{}
+pub trait Operation<'a>: std::fmt::Debug {}
+pub trait Operand<'a>: std::fmt::Debug + AsDynOperand<'a> {}
 pub trait Location<'a>: Operand<'a> {}
 impl<'a, T: ?Sized + Location<'a>> Operand<'a> for T {}
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct Program<'a: 'b, 'b>(Vec<Box<dyn Operation<'a> + 'b>>);
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct Add<'a: 'b, 'b> {
-    result: Box<dyn Location<'a> + 'b>,
-    lhs: Box<dyn Operand<'a> + 'b>,
-    rhs: Box<dyn Operand<'a> + 'b>,
+pub trait AsDynOperand<'a> {
+    fn as_dyn_operand(self: Box<Self>) -> Box<dyn Operand<'a> + 'a>;
 }
-impl<'a: 'b, 'b> Operation<'a> for Add<'a, 'b> {}
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct Sub<'a: 'b, 'b> {
-    result: Box<dyn Location<'a> + 'b>,
-    lhs: Box<dyn Operand<'a> + 'b>,
-    rhs: Box<dyn Operand<'a> + 'b>,
+impl<'a, T: /* Sized + */ Operand<'a> + 'a> AsDynOperand<'a> for T {
+    fn as_dyn_operand(self: Box<Self>) -> Box<dyn Operand<'a> + 'a> {
+        self
+    }
 }
-impl<'a: 'b, 'b> Operation<'a> for Sub<'a, 'b> {}
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+
+
+#[derive(Debug)]
+struct Program<'a>(Vec<Box<dyn Operation<'a> + 'a>>);
+
+#[derive(Debug)]
+struct Add<'a> {
+    result: Box<dyn Location<'a> + 'a>,
+    lhs: Box<dyn Operand<'a> + 'a>,
+    rhs: Box<dyn Operand<'a> + 'a>,
+}
+impl<'a> Operation<'a> for Add<'a> {}
+
+#[derive(Debug)]
+struct Sub<'a> {
+    result: Box<dyn Location<'a> + 'a>,
+    lhs: Box<dyn Operand<'a> + 'a>,
+    rhs: Box<dyn Operand<'a> + 'a>,
+}
+impl<'a> Operation<'a> for Sub<'a> {}
+
+#[derive(Debug)]
 struct Register<'a>(&'a str);
 impl<'a> Location<'a> for Register<'a> {}
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug)]
 struct Global<'a>(&'a str);
 impl<'a> Location<'a> for Global<'a> {}
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug)]
 struct Literal(i32);
 impl<'a> Operand<'a> for Literal {}
 
 parser!{
-grammar assembly<'b>() for str {
-    pub rule program() -> Program<'input, 'b>
+grammar assembly() for str {
+    pub rule program() -> Program<'input>
         = op:operation() ** "\n" { Program(op) }
 
     rule _ = [' ']*
 
-    rule operation() -> Box<dyn Operation<'input> + 'b>
-        = add() / sub()
+    rule operation() -> Box<dyn Operation<'input> + 'input>
+        = a:add() {a} / s:sub() {s}
 
-    rule add() -> Box<Add<'input, 'b>>
+    rule add() -> Box<Add<'input>>
         = result:location() _ "=" _ "add" _ lhs:operand() _ rhs:operand() { Box::new(Add{ result, lhs, rhs }) }
 
-    rule sub() -> Box<Sub<'input, 'b>>
+    rule sub() -> Box<Sub<'input>>
         = result:location() _ "=" _ "sub" _ lhs:operand() _ rhs:operand() { Box::new(Sub{ result, lhs, rhs }) }
 
-    rule location() -> Box<dyn Location<'input> + 'b>
-        = register() / global()
+    rule location() -> Box<dyn Location<'input> + 'input>
+        = r:register() {r} / g:global() {g}
 
     rule register() -> Box<Register<'input>>
         = "%" _ id:identifier() { Box::new(Register(id)) }
@@ -66,42 +78,43 @@ grammar assembly<'b>() for str {
     rule identifier() -> &'input str
         = $(['a'..='z' | 'A'..='Z']+)
 
-    rule operand() -> Box<dyn Operand<'input> + 'b>
-        = location() / literal()
+    rule operand() -> Box<dyn Operand<'input> + 'input>
+        = l:location() {l.as_dyn_operand()} / l:literal() {Box::new(l)}
     
-    rule literal() -> Box<Literal>
+    rule literal() -> Literal
         = n:$(['0'..='9']+) {?
             let n = n.parse::<i32>().map_err(|_| "invalid int literal")?;
-            Ok(ast::Constant::Integer(n))
+            Ok(Literal(n))
         }
 
 }}
 
 fn main() {
-    let Program(ops) = assembly::program("%apple = add 1 @g
+    let parsed = assembly::program("%apple = add 1 @g
 @b = add 2 %a
 %c = sub 82 @b
 @dog = sub @b 12").unwrap();
-    let iter = ops.into_iter();
-    assert_eq!(iter.next(), Some(Add{
-        result: Box::new(Register("apple")),
-        lhs: Box::new(Literal(1)),
-        rhs: Box::new(Global("g"))
-    }));
-    assert_eq!(iter.next(), Some(Add{
-        result: Box::new(Global("b")),
-        lhs: Box::new(Literal(2)),
-        rhs: Box::new(Register("a"))
-    }));
-    assert_eq!(iter.next(), Some(Sub{
-        result: Box::new(Register("c")),
-        lhs: Box::new(Literal(82)),
-        rhs: Box::new(Global("b"))
-    }));
-    assert_eq!(iter.next(), Some(Sub{
-        result: Box::new("dog"),
-        lhs: Box::new(Global("b")),
-        rhs: Box::new(Literal(12))
-    }));
-    assert_eq!(iter.next(), None);
+    let expected = Program(vec![
+        Box::new(Add{
+            result: Box::new(Register("apple")),
+            lhs: Box::new(Literal(1)),
+            rhs: Box::new(Global("g"))
+        }),
+        Box::new(Add{
+            result: Box::new(Global("b")),
+            lhs: Box::new(Literal(2)),
+            rhs: Box::new(Register("a"))
+        }),
+        Box::new(Sub{
+            result: Box::new(Register("c")),
+            lhs: Box::new(Literal(82)),
+            rhs: Box::new(Global("b"))
+        }),
+        Box::new(Sub{
+            result: Box::new(Global("dog")),
+            lhs: Box::new(Global("b")),
+            rhs: Box::new(Literal(12))
+        }),
+    ]);
+    assert_eq!(format!("{parsed:?}"), format!("{expected:?}"));
 }
