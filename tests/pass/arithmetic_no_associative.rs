d4462211506baa1ffa863bb1e299ed29f7e66c00
Kevin Mehall <km@kevinmehall.net>
2025-11-02 13:21:56 -0700

    arithmetic_no_associative: add failing test case

--- tests/pass/arithmetic_no_associative.rs
+++ tests/pass/arithmetic_no_associative.rs
@@ -2,7 +2,10 @@ extern crate peg;
 
 peg::parser!(grammar arithmetic() for str {
     pub(crate) rule no_assoc() = precedence! {
-        @ "+" @ {}
+        (@) "||" @ {}
+        --
+        @ "<" @ {}
+        @ ">" @ {}
         --
         @ "?" {}
         --
@@ -14,26 +17,33 @@ peg::parser!(grammar arithmetic() for str {
 
 #[test]
 fn main() {
-    assert!(arithmetic::no_assoc("x+x").is_ok());
+    assert!(arithmetic::no_assoc("x<x").is_ok());
+    assert!(arithmetic::no_assoc("x>x").is_ok());
     assert!(arithmetic::no_assoc("-x").is_ok());
     assert!(arithmetic::no_assoc("x?").is_ok());
 
-    assert!(arithmetic::no_assoc("-x?").is_ok());
-    assert!(arithmetic::no_assoc("x+-x").is_ok());
-    assert!(arithmetic::no_assoc("-x+x").is_ok());
-    assert!(arithmetic::no_assoc("-x+-x").is_ok());
-    assert!(arithmetic::no_assoc("x+x?").is_ok());
-    assert!(arithmetic::no_assoc("x?+x").is_ok());
-    assert!(arithmetic::no_assoc("x?+x?").is_ok());
-    assert!(arithmetic::no_assoc("x+-x?").is_ok());
-    assert!(arithmetic::no_assoc("x?+-x?").is_ok());
-    assert!(arithmetic::no_assoc("-x?+-x?").is_ok());
+    assert!(arithmetic::no_assoc("x||x").is_ok());
+    assert!(arithmetic::no_assoc("x<x||x").is_ok());
+    assert!(arithmetic::no_assoc("x||x<x").is_ok());
+    assert!(arithmetic::no_assoc("x<x||x<x").is_ok());
 
-    assert!(arithmetic::no_assoc("x+x+x").is_err());
-    assert!(arithmetic::no_assoc("x?+x+x").is_err());
-    assert!(arithmetic::no_assoc("-x+x+x").is_err());
-    assert!(arithmetic::no_assoc("x+-x+x").is_err());
-    assert!(arithmetic::no_assoc("x+x?+x").is_err());
-    assert!(arithmetic::no_assoc("x+x?+-x").is_err());
-    assert!(arithmetic::no_assoc("x+x?+x?").is_err());
+    assert!(arithmetic::no_assoc("-x?").is_ok());
+    assert!(arithmetic::no_assoc("x<-x").is_ok());
+    assert!(arithmetic::no_assoc("-x<x").is_ok());
+    assert!(arithmetic::no_assoc("-x<-x").is_ok());
+    assert!(arithmetic::no_assoc("x<x?").is_ok());
+    assert!(arithmetic::no_assoc("x?<x").is_ok());
+    assert!(arithmetic::no_assoc("x?<x?").is_ok());
+    assert!(arithmetic::no_assoc("x<-x?").is_ok());
+    assert!(arithmetic::no_assoc("x?<-x?").is_ok());
+    assert!(arithmetic::no_assoc("-x?<-x?").is_ok());
+
+    assert!(arithmetic::no_assoc("x<x<x").is_err());
+    assert!(arithmetic::no_assoc("x<x>x").is_err());
+    assert!(arithmetic::no_assoc("x?<x<x").is_err());
+    assert!(arithmetic::no_assoc("-x<x<x").is_err());
+    assert!(arithmetic::no_assoc("x<-x<x").is_err());
+    assert!(arithmetic::no_assoc("x<x?<x").is_err());
+    assert!(arithmetic::no_assoc("x<x?<-x").is_err());
+    assert!(arithmetic::no_assoc("x<x?<x?").is_err());
 }
